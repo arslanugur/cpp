e382b4a84d075869e88f517dcab7769ebe48a18e
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-09-20 02:19:58 +0300

    Create 06-basic-arithmetic.cpp

---
+++ 06-basic-arithmetic.cpp
@@ -0,0 +1,35 @@
+Syntax Tips for Arithmetic Operators
+/*
+Operators       |Symbols| Form
+-------------------------------
+Addition        | +     | x + y
+Subtraction     | -     | x - y
+Multiplication  | *     | x * y
+Division        | /     | x / y
+Modulus         | %     | x % y   (Modulus = Integer Remainder)
+
+Basic Assignment : x = y 
+Unary plus : +x 
+Unary minus : -x 
+Increment prefix : ++x postfix : x++ 
+Decrement prefix : --x postfix : x-- 
+Comparison operator
+Logical operator
+Compound assignment operator
+Bitwise operator
+...
+*/
+
+
+
+      //The addition operator adds its operands together.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 40 + 60;
+    cout << x;
+    return 0;
+}
+      //You can use multiple arithmetic operators in one line.
