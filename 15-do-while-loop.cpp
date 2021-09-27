779808022826d89b347ccbc175b7051f5b262b5c
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-09-27 11:10:05 +0200

    Update 15-do-while-loop.cpp

--- 15-do-while-loop.cpp
+++ 15-do-while-loop.cpp
@@ -1,1 +1,27 @@
+The do...while Loop
+      //Unlike for and while loops, which test the loop condition at the top of the loop, 
+      //the do...while loop checks its condition at the bottom of the loop.
+      //A do...while loop is similar to a while loop. 
+      //The one difference is that the do...while loop is guaranteed to execute at least one time.
+      //Syntax:
+do {
+   statement(s);
+} while (condition);
 
+      //Syntax:
+do { } while(test);
+
+      //Example:
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    do {
+        cout << a << endl;
+        a++;
+    } while(a < 5);
+
+    return 0;
+}
