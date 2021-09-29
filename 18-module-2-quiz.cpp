f349d486aaa5b38c3f1b6c9c44b979ff31b80ce1
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-09-29 16:42:28 +0300

    Create 18-module-2-quiz.cpp

---
+++ 18-module-2-quiz.cpp
@@ -0,0 +1,49 @@
+      //Example: to enter a value and store in the variable x, 
+      //then test if it's greater than 20, 
+      //print "x is big" on the screen, if x is less than 10, print "x is small".
+int x;
+cin >> x;
+if (x > 20) {
+ cout << "x is big" << endl;
+} 
+if (x < 10) {
+ cout << "x is small" << endl;
+}
+
+      //Example
+if (x>10 && x<20) { 
+  cout << "we are only friends"; }
+
+
+      //Example: to print "in a loop" to the screen 5 times using the while loop.
+int x = 1;
+while (x <= 5) {
+ cout << "in a loop" << endl;
+x++;
+}     //First value is 5, because loop start the 1to 5 time 
+      //Secound x Because this is incriment 1 just like x++
+
+      //Example: to print "in a loop" to the screen 7 times using the for loop.
+for (int x = 0; 
+x < 7; x++) {
+  cout << "in a loop" << endl;
+}
+
+      /*True about the switch statement     --> switch = if
+        -The switch statement may be an alternative for multiple if statements */
+
+      /*the correct statements about && and || operators.
+        -a || b is true, if either a or b is true
+        -(a || b) && c is true, if c is true and either a or b is true*/
+
+
+      //to print "You rock!" to the screen if variable a is greater than 12, and variable b is less than or equal to 76.
+int a = 144;
+int b = 33;
+
+if (a > 12 && b <= 76) {
+  cout << "You rock!" << endl;
+}
+
+
+
