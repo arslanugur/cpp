2ef8e05bae8629fd29d9d488f52fb35dfe62c155
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-08 16:28:11 +0300

    Create 09-while-loop.cpp

---
+++ programs/09-while-loop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+// Local variable declaration:
+int a = 1;
+
+// while loop execution
+while( a < 6 );
+{
+cout <<"value of a: " << a << endl;
+a = a + 1;
+}
+
+return 0;
+}
+/*Output:value of a: 1
+         value of a: 2
+         value of a: 3
+         value of a: 4
+         value of a: 5*/   
+
