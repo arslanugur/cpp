ec59b9d71da9d92c9d9aacb7c09ec98186893259
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-08 16:20:35 +0300

    Create 02-addition.cpp

---
+++ programs/02-addition.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+int main()
+{
+int first, second, sum;
+
+cout<< "enter two ints to add\n";
+cin>>first>>;
+
+sum = first + second; //adding contents of first and second and storing in sum
+
+cout<< "Sum of entered numbers = << sum<<\n";
+return 0;
+}
+
