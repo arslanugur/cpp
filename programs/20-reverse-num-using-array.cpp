adc88a250fabdbfd2ae96389697c63a08852ed8e
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-08 16:54:26 +0300

    Create 20-reverse-num-using-array.cpp

---
+++ programs/20-reverse-num-using-array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n, array[100], i;
+cout<<"Enter no of digits in your number";
+cin>>n;
+
+cout<<"Enter no.";
+for(i=0,i < n;i++)
+cin>>a[i];
+/storing each no in array/
+
+cout<<"Reverse no is : ";
+for(i=n-1,i > =0;i--)
+cout<<a[i];
+/Printing array in reverse order/
+}
+
+/*
+Output:
+Enter number of digits in your number 2                                                
+Enter numbers: 5 7                                                                       
+Reverse number is : 7 5
+*/
+
