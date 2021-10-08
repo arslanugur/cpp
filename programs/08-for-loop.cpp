d13ecffa62311e6b6f40c34ab5761e4615f8aa58
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-08 16:27:32 +0300

    Create 08-for-loop.cpp

---
+++ programs/08-for-loop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+// Local variable declaration:
+int a = 1;
+
+// while loop execution
+for(a=1; a<6; a++) {
+cout <<"value of a: " << a << endl;
+}
+
+return 0;
+}
+/*Output:
+    value of a: 1
+    value of a: 2
+    value of a: 3
+    value of a: 4
+    value of a: 5*/
+
