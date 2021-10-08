a828c98a8579c1391863289da83d28257da2484c
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-08 16:37:09 +0300

    Create 10-do-while-loop.cpp

---
+++ programs/10-do-while-loop.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+// Local variable declaration:
+int a = 1;
+
+// do loop execution
+do
+{
+cout <<"value of a: " << a << endl;
+a = a + 1;
+}while( a < 6 );
+
+return 0;
+}
+/*Output:
+value of a: 1
+value of a: 2
+value of a: 3
+value of a: 4
+value of a: 5*/
+
