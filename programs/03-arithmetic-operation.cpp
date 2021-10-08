75a27bb75a87a523314f7fd5ef10764d3c2b1a10
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-08 16:21:53 +0300

    Create 03-arithmetic-operation.cpp

---
+++ programs/03-arithmetic-operation.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int first, second, add, subtract, multiply;
+float divide;
+
+cout<<"Enter two integers\n";
+cin>>first>>second;
+
+add = first + second;
+subtract = first - second;
+multiply = first * second;
+divide = first / (float)second;     //typecasting
+
+cout<<"Sum = "<<add<<"\n";
+cout<<"Difference ="<<subtract<<"\n";
+cout<<"Multiplication ="<<multiply<<"\n";
+cout<<"Division ="<<divide<<"\n";
+
+return 0;
+} 
+
