7d3e9619d272b5b1de416f73320b2030616da364
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-08 16:23:04 +0300

    Create 04-if-statement.cpp

---
+++ programs/04-if-statement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int a = 10;
+
+// check for condition
+if( a > 5 )
+{
+// if condition is true then print the following
+
+cout<< " If Condition is satisfied ";
+cout << " a is greater than 5 " << endl;
+}
+cout << "value of a is : " << a << endl;
+
+return 0;
+}
+/*Output: If Condition is satisfied
+          a is greater than 5
+          value of a is : 10  */
+
