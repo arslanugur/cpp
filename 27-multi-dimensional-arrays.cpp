943925019252e0ca4ed1478c4b6731103b2f1713
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-11 09:51:53 +0300

    Update 27-multi-dimensional-arrays.cpp

--- 27-multi-dimensional-arrays.cpp
+++ 27-multi-dimensional-arrays.cpp
@@ -1,1 +1,16 @@
+Multi-Dimensional Arrays
+      //A multi-dimensional array holds one or more arrays. Declare a multidimensional array as follows.
+type name[size1][size2]...[sizeN];
+
+      //Here, we've created a two-dimensional 3x4 integer array:
+int x[3][4];
+
+      //Visualize this array as a table composed of 3 rows, and 4 columns.
+/*     	|  Coloumn 1 | Coloumn 2 | Coloumn 3 | Coloumn 4 |
+Row 1   |  x[0]0[]   |  x[0][1]  |  x[0][2]  |  x[0][3]  |
+Row 2   |  x[1][0]   |  x[1][1]  |  x[1][2]  |  x[1][3]  |
+Row 3   |  x[2][0]   |  x[2][1]  |  x[2][2]  |  x[2][3]  |*/
+
+      //Remember element counting always starts from 0.
+
 
