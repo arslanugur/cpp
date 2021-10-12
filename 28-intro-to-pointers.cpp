a57d05f171d4715de8bab1c0deaeae8e850278c2
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-12 16:08:55 +0300

    Update 28-intro-to-pointers.cpp

--- 28-intro-to-pointers.cpp
+++ 28-intro-to-pointers.cpp
@@ -1,1 +1,22 @@
+POINTERS
+      //Every variable is a memory location, which has its address defined.
+      //That address can be accessed using the ampersand (&) operator (also called the address-of operator), which denotes an address in memory.
+
+      //For example:
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int score = 5;
+    cout << &score << endl;
+
+    return 0;
+}   //output: 0x7ffe47d95ebc
+
+      //This outputs the memory address, which stores the variable score.
+
+
+
+
 
