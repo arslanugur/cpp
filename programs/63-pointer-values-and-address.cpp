733fcb4c7cc7e14327f5e5cdc613da96120d7a62
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-15 00:48:12 +0300

    Create 63-pointer-values-and-address.cpp

---
+++ programs/63-pointer-values-and-address.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int var = 10; // Variable Declaration
+int *ip; // variable for Pointer
+
+ip = &var; // address of variable is stored in pointer variable
+
+cout << "Value of var variable: ";
+cout << var << endl; //Print value of variable
+
+// Print the address of variable
+cout << "Address stored in ip variable: ";
+cout << ip << endl;
+
+// access the value at the address available in pointer
+cout << "Value of *ip variable: ";
+cout << *ip << endl;
+
+return 0;
+}
+
+/*
+Output:
+Value of var variable: 10
+Address stored in ip variable: 0x22ff44
+Value of *ip variable: 10
+*/
+
