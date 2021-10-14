979ba5d473abd87921417324f3426e35b258ed1f
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-14 20:09:21 +0300

    Create 35-complex-number-using-classes.cpp

---
+++ programs/35-complex-number-using-classes.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private: int real;
+int imag;
+public:
+Complex(int r = 0, int i = 0):
+real(r),
+imag(i) {};
+
+void setComplex(void) {
+cout << "Enter the real and imaginary parts : ";
+cin >> this->real;
+cin >> this->imag;
+
+}
+
+Complex add(const Complex& c) {
+Complex comp;
+comp.real = this->real + c.real;
+comp.imag = this->imag + c.imag;
+return comp;
+}
+
+Complex subtract(const Complex& c) {
+Complex comp;
+comp.real = this->real - c.real;
+comp.imag = this->imag - c.imag;
+return comp; }
+
+void printComplex(void) {
+cout << "Real : " << this->real << endl << "Imaginary : " << this->imag << endl;
+}   };
+
+int main() {
+Complex a, b, c, d;
+cout << "Setting first complex number " << endl;
+a.setComplex();
+cout << "Setting second complex number " << endl;
+b.setComplex();
+/* Adding two complex numbers */
+cout << "Addition of a and b : " << endl;
+c = a.add(b); c.printComplex();
+/* Subtracting two complex numbers */
+cout << "Subtraction of a and b : " << endl;
+d = a.subtract(b); d.printComplex();
+}
+
+/*
+Output:
+Setting first complex number
+Enter the real and imaginary parts : 1 1
+Setting second complex number
+Enter the real and imaginary parts : 2 2
+Addition of a and b : Real : 3 Imaginary : 3
+Subtraction of a and b : Real : -1 Imaginary : -1
+*/
+
