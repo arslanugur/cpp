4dbacd072f7da699a5cdfffaeb411c1d0cd58c3c
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-15 00:56:10 +0300

    Create 67-dynamic-memory-allocation(constructor-and-destructor).cpp

---
+++ programs/67-dynamic-memory-allocation(constructor-and-destructor).cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Cube
+{
+public:
+Cube() {
+cout << "Constructor is called!" <<endl;
+}
+~Cube() {
+cout << "Destructor is called!" <<endl;
+}
+};
+
+int main( )
+{
+Cube* myCubeArray = new Cube[5];
+
+delete [] myCubeArray; // Delete array
+
+return 0;
+}
+
+/*
+Output:
+Constructor is called!
+Constructor is called!
+Constructor is called!
+Constructor is called!
+Constructor is called!
+Destructor is called!
+Destructor is called!
+Destructor is called!
+Destructor is called!
+Destructor is called!*/
+
