6ebed84a13af4fb33285415a2da972710bfae51e
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-14 21:08:15 +0300

    Create 51-function-overloading(static-polymorphism).cpp

---
+++ programs/51-function-overloading(static-polymorphism).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class printData
+{
+public:
+void print(int i) {
+cout << "Displaying the int : " << i << endl;
+}
+
+void print(double f) {
+cout << "Displaying the float : " << f << endl;
+}
+
+void print(char* c) {
+cout << "Displaying the character : " << c << endl;
+}
+};
+
+int main(void)
+{
+printData pd;
+
+
+pd.print(5); // Call print to print integer
+
+pd.print(500.263); // Call print to print float
+
+pd.print("Hello C++"); // Call print to print character
+
+return 0;
+}
+
+/*
+Output:
+Displaying the int : 5
+Displaying the float : 500.263
+Displaying the character : Hello C++
+*/
+
