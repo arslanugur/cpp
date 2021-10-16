270bee7a09e26cfa78967be448a571a8925cb1a5
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-17 01:01:43 +0300

    Create 115-#define-macro.cpp

---
+++ programs/115-#define-macro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14159
+
+int main ()
+{
+cout << "Value of PI :" << PI << endl;
+return 0;
+}
+<
+
+/*
+Output:
+Value of PI :3.14159
+*/
+  
+  
