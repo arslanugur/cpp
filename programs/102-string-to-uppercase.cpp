943e5a602b07d3aa06a6acf98a8a4e07118b5d0d
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-17 00:43:30 +0300

    Create 102-string-to-uppercase.cpp

---
+++ programs/102-string-to-uppercase.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+char string[] = "codetoinvent";
+
+cout<<strupr(string)<<"\n";
+/strupr() will print string in upper case/
+
+return 0;
+}
+
+/*
+Output:
+codetoinvent  //lower case
+CODETOINVENT  //upper case
+*/
+
