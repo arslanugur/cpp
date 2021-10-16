03e621f51186d8210cfd34fb13176ba39d5a8477
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-16 15:10:21 +0300

    Create 83-decimal-to-octal.cpp

---
+++ programs/83-decimal-to-octal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+long num, decimal_num, remainder, base = 1, octal = 0;
+
+cout<<"Enter a decimal integer \n";
+cin>>num;
+decimal_num = num;
+while (num > 0)
+{
+remainder = num % 8;
+octal = octal + remainder * base;
+num = num / 8;
+base = base * 10;
+}
+cout<<"Input number is ="<<decimal_num<<"\n";
+cout<<"Its octal equivalent is ="<<octal<<"\n";
+}
+
+/*
+Output:
+Enter a decimal integer: 67                                                                                 
+Input number is =67                                                                
+Its octal equivalent is =103
+*/
+
