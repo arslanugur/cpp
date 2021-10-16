e187ffd9af783b2a92825385bcc6686456c92328
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-17 00:42:04 +0300

    Create 101-string-to-lowercase.cpp

---
+++ programs/101-string-to-lowercase.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+char string[] = "CODETOINVENT";
+
+cout<<strlwr(string)<<"\n";
+/strlwr() will print string in LOWER case/
+
+return 0;
+}
+
+/*
+Output:
+CODETOINVENT
+codetoinvent
+*/
+
