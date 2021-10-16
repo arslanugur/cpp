a3c2176f0224cf9c9e67c29c13a6309655a61e5d
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-16 14:54:03 +0300

    Create 75-mathematical-functions.cpp

---
+++ programs/75-mathematical-functions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+int a = 100;
+int b = -75;
+float c = 243.16;
+float d = 379.51;
+
+// mathematical operations;
+cout << "sin(a) :" << sin(a) << endl;
+cout << "abs(b) :" << abs(b) << endl;
+cout << "floor(c) :" << floor(c) << endl;
+cout << "sqrt(d) :" << sqrt(d) << endl;
+cout << "pow( d, 2) :" << pow(d, 2) << endl;
+
+return 0;
+}
+
+/*
+Output:
+sign(d) :-0.506366
+abs(i) :75
+floor(d) :243
+sqrt(f) :19.481
+pow( d, 2 ) :144028
+*/
+
+
