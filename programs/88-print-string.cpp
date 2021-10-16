7791215b1e186c8f3553ee5c03221285b8e415b1
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-16 15:24:50 +0300

    Create 88-print-string.cpp

---
+++ programs/88-print-string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char array[100];
+
+cout<<"Enter a string\n";
+cin>>array;
+/* %s is used to retrive string from user*/
+
+cout<<"You entered the string"<<array<<"\n";
+return 0;
+}
+
+/*
+Output:
+Enter a string codetoinvent
+You entered the string codetoinvent
+*/
+
