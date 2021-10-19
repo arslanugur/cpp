5d916afb120fa8710e0b2c7cf821e853c1d4b0d9
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-19 06:31:24 +0300

    Create 127-int-datatype.cpp

---
+++ programs/127-int-datatype.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+
+cout<<"Enter an integer\n";
+cin>>a;
+//takes an integer from user
+
+cout<<"Integer that you have entered is"<<a<<"\n";
+
+return 0;
+}
+
+/*
+Output:
+Enter an integer
+10
+Integer that you have entered is 10
+*/
+
