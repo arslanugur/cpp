e0651ee982fa54314b32b3053826c75de6c4166d
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-19 06:26:08 +0300

    Create 122-global-and-local-variables.cpp

---
+++ programs/122-global-and-local-variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Global variable declaration:
+int a = 20;
+
+int main ()
+{
+// Local variable declaration:
+int a = 10;
+
+cout << a;
+
+return 0;
+}
+
+//Output: 10
+
