6686b384f1b90d8af452999e439c72ccae6e7213
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-21 21:39:32 +0300

    Create 04-endl-keyword.cpp

---
+++ programs/basic/04-endl-keyword.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    num = 50;
+
+    cout << "number is=" << num << endl;
+    //endl is manipulator
+    //endl is output manipulator
+    cout << "value of number=" << num << "\n";
+}
+
+/*
+Output:
+number is=50
+value of number=50
+*/
+
