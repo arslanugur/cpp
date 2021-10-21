483c5fae5942320f69a970885e313add6141ce99
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-21 21:40:40 +0300

    Create 05-find-ASCII-value-of-a-character.cpp

---
+++ programs/basic/05-find-ASCII-value-of-a-character.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //variable declaration
+    char asciichar;
+
+    //take user input
+    cout << "Enter a character : ";
+    cin >> asciichar;
+
+    //display ascii value of character
+    cout << "\nIt's ascii  value is : " << (int) asciichar << endl;
+
+    return 0;
+}
+
+/*
+Output:
+Enter a character : a
+It's ascii  value is : 97
+*/
+
