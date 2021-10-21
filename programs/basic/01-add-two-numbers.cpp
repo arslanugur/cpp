c956280cc9d5be391a6d9ebdf447207cc447f89f
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-21 21:42:40 +0300

    Update and rename 01.add-two-numbers.cpp to 01-add-two-numbers.cpp

---
+++ programs/basic/01-add-two-numbers.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //variable declaration
+    int num1, num2, result;
+
+    //take user input
+    cout << "Enter first Number : ";
+    cin >> num1;
+
+    cout << "\nEnter second Number : ";
+    cin >> num2;
+
+    //to add two number
+    result = num1 + num2;
+
+    //display addition
+    cout << "\nAddition is : " << result;
+
+    return 0;
+}
+
+/*
+Output:
+Enter first Number : 2 3
+Enter second Number :
+Addition is : 5
+*/
+
