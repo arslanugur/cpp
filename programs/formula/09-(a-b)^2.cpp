e82b55c10dc95b78773d98d63f6efbb75adac68e
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-22 11:25:09 +0300

    Create 09-(a-b)^2.cpp

---
+++ programs/formula/09-(a-b)^2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Mathematics
+{
+  int a, b;
+
+	public:
+	void input()
+	{
+		cout << "Input a:\n";
+		cin >> a;
+		cout << "Input b:\n\n";
+		cin >> b;
+	}
+
+	void add()
+	{
+		cout << "(a-b)^2 = " << ((a*a)-(2*a*b)+(b*b));
+	}
+ };
+
+int main()
+{
+   Mathematics m; // Creating object of class
+
+   m.input();
+   m.add();
+
+   return 0;
+}
+
+/*
+Output:
+Input a: 50
+Input b: 10
+(a-b)^2 = 1600
+*/
+
