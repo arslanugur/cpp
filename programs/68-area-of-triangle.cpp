2dac4e3b4358dc5cd45da4598d7bbd3b2b4ccbf3
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-22 10:13:59 +0300

    Update and rename 68- to 68-area-of-triangle.cpp

---
+++ programs/68-area-of-triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int height, base;
+float ans;/ans may come in fractions/
+
+cout<<"Enter height and base";
+cin>>height>>base;
+ans= (1/2)*height*base;
+/* mathematical formula*/
+
+cout<<"Area if triangle is"<<ans;
+} 
+
+/*
+Output:
+Enter height and base 4                                                             
+6                                                                                  
+Area if triangle is 12
+*/
+
+
