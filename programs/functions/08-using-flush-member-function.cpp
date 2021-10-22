f52aa9e59c3d786aed7a0aaf0fa2a394137e39f8
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-22 12:19:41 +0300

    Create 08-using-flush-member-function.cpp

---
+++ programs/functions/08-using-flush-member-function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "hey hello\n";
+    cout << "you have many letters to read in letter box\n";
+    //flush clears the output buffer
+    cout.flush();
+
+    return 0;
+}
+
+/*
+Output:
+hey hello
+you have many letters to read in letter box
+*/
+
