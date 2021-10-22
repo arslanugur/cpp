1e14ead8fe530ab4667ff0c4e65b724c9fac9f3a
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-22 09:51:29 +0300

    Update and rename 57-stack-using-array.cpp to 057-stack-using-array.cpp

--- programs/057-stack-using-array.cpp
+++ programs/057-stack-using-array.cpp
@@ -1,9 +1,11 @@
-/*A stack is an abstract data structure that contains a collection of elements. 
+/*
+  A stack is an abstract data structure that contains a collection of elements. 
   Stack implements the LIFO mechanism i.e. the element that is pushed at the end is popped out first. 
   Some of the principle operations in the stack are -
 * Push - This adds a data value to the top of the stack.
 * Pop - This removes the data value on top of the stack
-* Peek - This returns the top data value of the stack A program that implements a stack using array is given as follows.*/
+* Peek - This returns the top data value of the stack A program that implements a stack using array is given as follows.
+*/
 
 #include <iostream>
 using namespace std;
