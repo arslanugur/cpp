d20d8027844e883f0f9d28a1392b41cd98442a4e
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-22 12:29:32 +0300

    Create 03-do-while-loop.cpp

---
+++ programs/loop/03-do-while-loop.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i = 1;
+
+    do
+    {
+        cout << "value of i: " << i << endl;
+        i++;
+    }
+    while (i <= 10);
+
+    return 0;
+}
+
+/*
+Output:
+value of i: 1
+value of i: 2
+value of i: 3
+value of i: 4
+value of i: 5
+value of i: 6
+value of i: 7
+value of i: 8
+value of i: 9
+value of i: 10
+*/
+
