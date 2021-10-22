12ddd96c95f235a00f4dfd866bb009c496122588
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-22 11:12:34 +0300

    Create 02-area-of-circle.cpp

---
+++ programs/formula/02-area-of-circle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r,a;
+
+    cout<<"Enter Radius : ";
+    cin>>r;
+
+    a=3.14*r*r;
+
+    cout<<"Area of Circle : "<<a;
+
+    return 0;
+}
+
+/*
+Output:
+Enter Radius : 3
+Area of Circle : 28.26
+*/
+
