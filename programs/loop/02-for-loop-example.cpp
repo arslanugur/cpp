67c213bba63042c3e0ac8f37be115d903adc167c
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-22 12:26:37 +0300

    Create 02-for-loop-example.cpp

---
+++ programs/loop/02-for-loop-example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // for loop execution
+    for (int i = 0; i <= 10; i++)
+    {
+        cout << "value of i: " << i << endl;
+    }
+
+    return 0;
+}
+
+/*
+Output:
+value of i: 0
+value of i: 1
+value of i: 2
+value of i: 3
+value of i: 4
+value of i: 5
+value of i: 6
+value of i: 7
+value of i: 8
+value of i: 9
+value of i: 10
+*/
+
