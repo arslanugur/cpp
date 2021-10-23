147c0baa7c5da3eccaff7a34cea4ae2507b040bd
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-23 18:35:01 +0300

    Create 13-leap-year.cpp

---
+++ programs/miscellaneous/13-leap-year.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+   //declare variable
+   int year;
+
+   //take user input for year
+   cout<<"Enter year : ";
+   cin>>year;
+
+   if(year%4==0 || year%400==0 || year%100==0)
+   cout<<"This is a leap year"<<endl;
+
+   else
+   cout<<"This is not a leap year"<<endl;
+
+   return 0;
+}
+
+/*
+Output:
+Enter year : 2016
+This is a leap year
+*/
+
