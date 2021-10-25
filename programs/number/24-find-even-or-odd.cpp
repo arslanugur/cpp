ff75be3d08250fb19a5d263b03bf116b42c21294
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-25 18:22:58 +0300

    Create 24-find-even-or-odd.cpp

---
+++ programs/number/24-find-even-or-odd.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter an integer : ";
+    cin>>n;
+    if(n % 2 == 0)
+    {
+        cout<<n<<" is even.";
+    }
+    else
+    {
+        cout << n << " is odd.";
+    }
+    return 0;
+}
+
+/*
+Output:
+Enter an integer : 7
+7 is odd.
+*/
+  
+  
