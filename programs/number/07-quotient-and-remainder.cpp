32cbfcf1350ce8a5509d3400a32f8753bf467aec
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-25 17:51:29 +0300

    Create 07-quotient-and-remainder.cpp

---
+++ programs/number/07-quotient-and-remainder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 33, b = 5;
+    cout << "Quotient = " << a / b << endl;
+    cout << "Remainder = "<< a % b << endl;
+    return 0;
+}
+
+/*
+Output:
+Quotient = 6
+Remainder = 3
+*/
+
