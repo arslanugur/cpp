dd53f00eb251599e242bb9f81efed8602f7fe7f4
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-25 18:11:21 +0300

    Create 18-reverse-of-number.cpp

---
+++ programs/number/18-reverse-of-number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int number, reverse = 0;
+
+cout<<"Input a Number to Reverse and press Enter: ";
+// Taking Input Number in variable number
+cin>> number;
+
+   while(number!= 0)
+   {
+      reverse = reverse * 10;
+      reverse = reverse + number%10;
+      number = number/10;
+   }
+   cout<<"New Reversed Number is:  "<<reverse;
+   return 0;
+}
+
+/*
+Output:
+Input a Number to Reverse and press Enter: 123
+New Reversed Number is:  321
+*/
+
+  
