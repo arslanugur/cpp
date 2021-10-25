e894b2350376bf55cf3431b2223d5bf344ffb122
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-25 18:33:27 +0300

    Create 27-largest-and-smallest-number.cpp

---
+++ programs/number/27-largest-and-smallest-number.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    //declaration of variables
+    int num1, num2, num3;
+    int smallest, largest;
+
+    //take input from user
+    cout << "Please enter 3 numbers : "<<"" ;
+    cin >> num1 >> num2 >> num3;
+
+    //assign initial value for comparison
+    smallest = num1;
+    largest = num1;
+
+    if (num2 > largest)
+        {
+           largest = num2;
+        }
+
+    if (num3 > largest)
+        {
+            largest = num3;
+        }
+
+    if (num2 < smallest)
+        {
+            smallest = num2;
+        }
+
+    if (num3 < smallest)
+        {
+            smallest = num3;
+        }
+
+        //display largest number and smallest number
+    cout << "largest: " << largest << "\nsmallest: " << smallest << "\n";
+}
+
+/*
+Output:
+Please enter 3 numbers : 88
+34
+54
+largest: 88
+smallest: 34
+*/
+  
+  
