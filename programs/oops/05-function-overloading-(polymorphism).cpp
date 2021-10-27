43a138d53be5c2e6097ccfbb2bc85dced8c98fab
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-27 11:09:21 +0300

    Create 05-function-overloading-(polymorphism).cpp

---
+++ programs/oops/05-function-overloading-(polymorphism).cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class Add
+{
+    public:
+    void add(int a,int b)
+    {
+        int c;
+        c=(a+b);
+        cout<<a<<" + "<<b<<" = "<<c<<endl;
+    }
+
+    void add(float a,float b)
+    {
+        float c;
+        c=(a+b);
+        cout<<a<<" + "<<b<<" = "<<c<<endl;
+    }
+
+    void add(int a,int b,int c)
+    {
+        int d;
+        d=(a+b+c);
+        cout<<a<<" + "<<b<<" + "<<c<<" = "<<d<<endl;
+    }
+};
+
+int main()
+{
+    Add a;
+    a.add(5,6);
+    a.add(7,8,9);
+    a.add(8.0f,9.0f);
+
+    return 0;
+}
+
+/*
+Output:
+5 + 6 = 11
+7 + 8 + 9 = 24
+8 + 9 = 17
+*/
+  
+
