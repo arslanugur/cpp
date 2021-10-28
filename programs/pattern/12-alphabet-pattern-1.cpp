46655d6a3393963220ad5adef5578e2614337cb6
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-28 16:28:25 +0300

    Create 12-alphabet-pattern-1.cpp

---
+++ programs/pattern/12-alphabet-pattern-1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    //variable declaration
+    int x, y, n;
+    char ch;
+    cout << "Number of rows : ";
+    cin >> n;
+
+    for (x = 1; x <= n; x++)
+    {
+        cout << "\n";
+        ch = 'A';
+        for (y = 1; y <= x; y++)
+        {
+            cout << ch;
+            ch++;
+        }
+    }
+    return 0;
+}
+
+/*
+Output:
+Number of rows : 9
+A
+AB
+ABC
+ABCD
+ABCDE
+ABCDEF
+ABCDEFG
+ABCDEFGH
+ABCDEFGHI
+*/
+
+
