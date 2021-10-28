b97daef7ae7361410cbbf5c657458cfcf369c6b5
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-28 16:13:12 +0300

    Create 02-half-pyramid-with-numbers.cpp

---
+++ programs/pattern/02-half-pyramid-with-numbers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    for (i = 1; i <= rows; ++i)
+    {
+        for (j = 1; j <= i; ++j)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+/*
+Output:
+Enter the number of rows: 5
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+*/
+  
+  
