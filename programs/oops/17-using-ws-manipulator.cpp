82fa09f7cdc58ec46c17fb639deaa06878bda187
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-28 15:57:47 +0300

    Create 17-using-ws-manipulator.cpp

---
+++ programs/oops/17-using-ws-manipulator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main ( )
+{
+    char empname [100];
+    cout<<"Enter a employee name : ";
+
+    //ws is use to add white spaces
+    cin>>ws;
+    cin>>empname;
+    cout<<"\n";
+    cout<<"Employee name = "<<empname;
+}
+
+/*
+Output:
+Enter a employee name : John
+Employee name = John 
+*/
+
