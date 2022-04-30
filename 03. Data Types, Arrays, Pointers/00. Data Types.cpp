e8527e7a8b5ea8c2c15540f2c9a793c1afd8e964
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-30 23:09:59 +0300

    Update and rename 00. Data Types (Built-In).cpp to 00. Data Types.cpp

---
+++ 03. Data Types, Arrays, Pointers/00. Data Types.cpp
@@ -0,0 +1,25 @@
+1. PRIMITIVE
+• Integer
+• Floating Point
+• Character
+• Boolean
+• Double Floating Point
+• Void
+• Wide Character
+
+
+2. DERIVED
+• Functions
+• Arrays
+• Pointers
+• Reference
+
+
+3. USER-DEFINED
+• Class
+• Union
+• Structure
+• Enum
+• Typedef
+
+
