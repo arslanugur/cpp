bda2875c851883c3c99afaa9b877662c748cbed2
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-03 18:01:43 +0300

    Update and rename 01. Introduction to 01. Introduction.cpp

---
+++ Hackerrank Problems/01. Introduction.cpp
@@ -0,0 +1,63 @@
+// 01. Say "Hello, World!" With C++
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
+
+
+
+// 02. Input and Output
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout << a + b + c;
+    return 0;
+}
+
+
+
+// 03. Basic Data Types
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%d\n%ld\n%c\n%f\n%lf\n", i, l, c, f, d);
+    
+    return 0;
+}
+
+
+
+// 04. Conditional Statements
+
+// 05. For Loop
+
+// 06. Functions
+
+// 07. Pointer
+
+// 08. Arrays Introduction
+
+// 09. Variable Sized Arrays
